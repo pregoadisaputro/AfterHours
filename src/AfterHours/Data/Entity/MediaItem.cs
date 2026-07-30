37e75e0a4d4734aa2fd5b2774b206296f8874332
anlucys <pregoadisaputroo@gmail.com>
2026-07-31 04:17:06 +0700

    remove property and initialized the date time
    - overview
    - runtime

--- src/AfterHours/Data/Entity/MediaItem.cs
+++ src/AfterHours/Data/Entity/MediaItem.cs
@@ -12,12 +12,10 @@ public class MediaItem
 
     public string? Title { get; set; }
     public MediaType MediaType { get; set; }
-    public string? Overview { get; set; }
     public string? PosterPath { get; set; }
     public string? BackdropPath { get; set; }
     public DateOnly? ReleaseDate { get; set; }
-    public int? Runtime { get; set; }
 
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
