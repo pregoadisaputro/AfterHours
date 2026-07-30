fb9c940bd485707521e41c5e3ed2547ea151eb08
anlucys <pregoadisaputroo@gmail.com>
2026-07-31 01:54:48 +0700

    setup tmdb dto

---
+++ src/AfterHours/Services/Tmdb/Dto/TmdbDto.cs
@@ -0,0 +1,32 @@
+namespace AfterHours.Services.Tmdb.Dto;
+
+public record TmdbDetailsResponse(
+    int Id,
+    string? Title,
+    string? Name,
+    string? Overview,
+    int? Runtime,
+    string? Release_Date,
+    string? First_Air_Date,
+    string? Poster_Path,
+    string? Backdrop_Path
+);
+
+public record TmdbSearchResponse(
+    int Page,
+    int Total_Pages,
+    int TotalResutls,
+    IReadOnlyList<TmdbSearchResults> Results
+);
+
+public record TmdbSearchResults(
+    int Id,
+    string? Title,
+    string? Name,
+    string? Media_Type,
+    string? Overview,
+    string? Release_Date,
+    string? First_Air_Date,
+    string? Poster_Path,
+    string? Backdrop_Path
+);
