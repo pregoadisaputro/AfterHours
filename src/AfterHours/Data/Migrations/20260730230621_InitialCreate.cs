92d250fae62264b8d5190dcf497bfc4234b152a4
anlucys <pregoadisaputroo@gmail.com>
2026-07-31 07:53:29 +0700

    initial database migration and update

---
+++ src/AfterHours/Data/Migrations/20260730230621_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AfterHours.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MediaItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExternalId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rating = table.Column<decimal>(type: "TEXT", nullable: true),
+                    MediaStatus = table.Column<int>(type: "INTEGER", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    MediaType = table.Column<int>(type: "INTEGER", nullable: false),
+                    PosterPath = table.Column<string>(type: "TEXT", nullable: true),
+                    BackdropPath = table.Column<string>(type: "TEXT", nullable: true),
+                    ReleaseDate = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MediaItems");
+        }
+    }
+}
