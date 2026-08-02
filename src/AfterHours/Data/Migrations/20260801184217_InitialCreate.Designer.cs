1020e58caf1a7957b115932ae43490308400c7e0
anlucys <pregoadisaputroo@gmail.com>
2026-08-02 20:36:06 +0700

    initial migration and database update

--- src/AfterHours/Data/Migrations/20260801184217_InitialCreate.Designer.cs
+++ src/AfterHours/Data/Migrations/20260801184217_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AfterHours.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260730230621_InitialCreate")]
+    [Migration("20260801184217_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
