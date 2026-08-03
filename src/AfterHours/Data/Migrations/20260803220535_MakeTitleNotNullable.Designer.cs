ff2a48d201b030cc71d706debdda4abaa96ef0ce
anlucys <pregoadisaputroo@gmail.com>
2026-08-04 05:06:28 +0700

    migration and update database for title not nullable

--- src/AfterHours/Data/Migrations/20260803220535_MakeTitleNotNullable.Designer.cs
+++ src/AfterHours/Data/Migrations/20260803220535_MakeTitleNotNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AfterHours.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AfterHours.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260803220535_MakeTitleNotNullable")]
+    partial class MakeTitleNotNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
---
+++ src/AfterHours/Data/Migrations/20260803220535_MakeTitleNotNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AfterHours.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeTitleNotNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "MediaItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "MediaItems",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
